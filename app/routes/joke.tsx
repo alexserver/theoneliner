034592fe45ba4e39fa3baa6c45ed156f29891e51
Alejandro Gomez <alexserverone@gmail.com>
2023-09-06 02:34:34 -0600

    changing the display of pics through css background

--- app/routes/joke.tsx
+++ app/routes/joke.tsx
@@ -1,6 +1,15 @@
 import supabase from "~/lib/supabase";
 import { json, type LoaderArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import styles from "~/styles/joke.css";
+
+function zeroPad(num: number, totalLength: number): string {
+  return String(num).padStart(totalLength, "0");
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export async function loader(params: LoaderArgs) {
   const { data: joke, error } = await supabase.rpc("one_joke").single();
@@ -11,5 +20,11 @@ export async function loader(params: LoaderArgs) {
 
 export default function () {
   const { joke } = useLoaderData();
-  return <div>{joke.content}</div>;
+  const picNumber = zeroPad(Math.floor(Math.random() * 11), 2);
+  const backgroundClass = `back pic${picNumber}`;
+  return (
+    <div className={backgroundClass}>
+      <h1 className="joke-content">{joke.content}</h1>
+    </div>
+  );
 }
