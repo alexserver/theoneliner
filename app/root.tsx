3b2e9b17d2551b367b5d368d7c0a94e97cc2e255
Alejandro Gomez <alexserverone@gmail.com>
2023-11-06 15:38:26 -0600

    Adding tailwind Adding an image components that loads random pics. Mabe will delegate this select random process to the page and not the component.

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
+import styles from "./tailwind.css";
 import {
   Links,
   LiveReload,
@@ -10,7 +11,8 @@ import {
 } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  // ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: styles },
 ];
 
 export default function App() {
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,22 +1,31 @@
-import type { V2_MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { json, type LoaderArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import styles from "~/styles/joke.css";
+import { prisma } from "~/lib/prisma.server";
+import Image from "~/components/Image";
 
-export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
-export default function Index() {
+export async function loader(params: LoaderArgs) {
+  const jokesCount = await prisma.joke.count();
+  const jokes = await prisma.joke.findMany({
+    take: 1,
+    skip: Math.floor(Math.random() * jokesCount),
+  });
+  const joke = jokes.at(0);
+  return json({ joke });
+}
+
+export default function () {
+  const { joke } = useLoaderData();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <Link to="/joke">Hit me</Link>
-        </li>
-      </ul>
+    <div className="w-full bg-gradient-to-b from-indigo-800 to-slate-900 p-4 h-screen flex justify-center items-center">
+      <div className="overflow-hidden absolute top-0 left-0 w-full max-h-full">
+        <Image />
+      </div>
+      <h1 className="joke-content">{joke.content}</h1>
     </div>
   );
 }
